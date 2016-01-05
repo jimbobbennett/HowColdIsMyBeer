4408873f29dd0746442ad2ee10074e3cfd6696a9
Jim Bennett <jimbobbennett@mac.com>
2016-01-05 14:48:17 +0000

    Testing other nearables

---
+++ AppDelegate.cs
@@ -0,0 +1,72 @@
+﻿using Estimote;
+using Foundation;
+using UIKit;
+
+namespace HowColdIsMyBeer
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        private TriggerManager _triggerManager;
+
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            var userNotificationSettings = UIUserNotificationSettings.GetSettingsForTypes(UIUserNotificationType.Alert, new NSSet());
+            UIApplication.SharedApplication.RegisterUserNotificationSettings(userNotificationSettings);
+
+            _triggerManager = new TriggerManager();
+            var wrongTemperatureRule = TemperatureRule.TemperatureBetween(2, 8, "841a40679cd3f7d4");
+            var tempTrigger = new Trigger(new Rule[] { wrongTemperatureRule }, "WrongTemperture");
+            _triggerManager.ChangedState += TriggerManagerOnChangedState;
+            _triggerManager.StartMonitoringForTrigger(tempTrigger);
+
+            return true;
+        }
+
+        private void TriggerManagerOnChangedState(object sender, TriggerChangedStateEventArgs args)
+        {
+            var notification = new UILocalNotification
+            {
+                AlertAction = "Your beer is at the wrong temperature!",
+                SoundName = UILocalNotification.DefaultSoundName
+            };
+
+            UIApplication.SharedApplication.PresentLocalNotificationNow(notification);
+        }
+
+        //
+        // This method is invoked when the application is about to move from active to inactive state.
+        //
+        // OpenGL applications should use this method to pause.
+        //
+        public override void OnResignActivation(UIApplication application)
+        {
+        }
+
+        // This method should be used to release shared resources and it should store the application state.
+        // If your application supports background exection this method is called instead of WillTerminate
+        // when the user quits.
+        public override void DidEnterBackground(UIApplication application)
+        {
+        }
+
+        // This method is called as part of the transiton from background to active state.
+        public override void WillEnterForeground(UIApplication application)
+        {
+        }
+
+        // This method is called when the application is about to terminate. Save data, if needed. 
+        public override void WillTerminate(UIApplication application)
+        {
+        }
+    }
+}
---
+++ RootViewController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using Estimote;
+using Foundation;
+using UIKit;
+
+namespace HowColdIsMyBeer
+{
+    public partial class RootViewController : UIViewController
+    {
+        private readonly NearableManager _nearableManager;
+        private readonly TriggerManager _triggerManager;
+        private readonly HashSet<string> _foundNearables = new HashSet<string>(); 
+
+        public RootViewController(IntPtr handle) : base(handle)
+        {
+            _nearableManager = new NearableManager();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        #region View lifecycle
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            TemperatureLabel.Text = "Locating...";
+
+            _nearableManager.RangedNearable += NearableManagerOnRangedNearable;
+            _nearableManager.RangedNearables += NearableManagerOnRangedNearables;
+            _nearableManager.StartRangingForIdentifier("a764931d560a883f");
+            _nearableManager.StartRangingForType(NearableType.Car);
+            _nearableManager.StartRangingForType(NearableType.Bed);
+        }
+
+        private void NearableManagerOnRangedNearables(object sender, DidRangeNearablesEventArgs didRangeNearablesEventArgs)
+        {
+            foreach (var nearable in didRangeNearablesEventArgs.Nearables)
+            {
+                _foundNearables.Add(nearable.Type.ToString());
+            }
+
+            Debug.WriteLine($"Found {_foundNearables.Count} nearables");
+        }
+
+        private void NearableManagerOnRangedNearable(object sender, DidRangeNearableEventArgs args)
+        {
+            const char degree = (char) 176;
+            TemperatureLabel.Text = $"{args.Nearable.Temperature:N}{degree}C";
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        #endregion
+    }
+}
